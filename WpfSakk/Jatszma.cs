b720f1b9e64f3a71b11f3538570539e415de182a
sinka.jozsef <sinka.jozsef@57-08>
2024-03-06 12:33:11 +0100

    Add project files.

---
+++ WpfSakk/Jatszma.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSakk
+{
+    public class Jatszma
+    {
+        static Dictionary<string, byte> dict = new()
+        {
+            {"a", 1},
+            {"b", 2},
+            {"c", 3},
+            {"d", 4},
+            {"e", 5},
+            {"f", 6},
+            {"g", 7},
+            {"h", 8}
+        };
+        
+
+        List<String> lepesek;
+        //todo Állapottér reprezentáció kialakítása V2.0
+
+        /// <summary>
+        /// Üres játék létrehozása
+        /// </summary>
+        public Jatszma()
+        {
+            lepesek = new List<String>();
+        }
+        public Jatszma(String fajlSor)
+        {
+            lepesek = new List<String>();
+            foreach (var item in fajlSor.Trim().Split('\t'))
+            {
+                lepesek.Add(item);
+            }
+        }
+
+        public int LepesekSzama => lepesek.Count();
+
+        public char Nyertes => LepesekSzama % 2 == 0 ? 's' : 'v';
+
+        //public int HuszarokLepesszama => lepesek.Count(lepes => lepes[0] == 'H');
+        public int HuszarokLepesszama => TisztLepesszama('H');
+
+        public int TisztLepesszama(char tisztJele)
+        {
+            return lepesek.Count(lepes => lepes[0] == tisztJele);
+        }
+
+        /// <summary>
+        /// todo: Keresse meg mindkét vezér (királynő) utolsó pozícióját és nézze meg, hogy ott ütötték-e ezt a pozíviót? (vmi x poz)
+        /// </summary>
+
+        public int BabukSzama => 32 - lepesek.Count(n => n.Contains('x'));
+
+        public bool FeherKiralyMozgas => FeherKiralyMozgott();
+
+        public bool FeherKiralyMozgott()
+        {
+            for (int i = 0; i < lepesek.Count(); i++) if (i%2==0 && (lepesek[i].Contains('K') || lepesek[i].Contains('O'))) return true;
+            return false;
+        }
+
+
+        public bool VezerLeutve()
+        {
+            string vilagosVezerPoz = "d1";
+            string sotetVezerPoz = "d8";
+            for (int i = 0; i < lepesek.Count(); i++)
+            {
+                if (lepesek[i].Contains('x') && (lepesek[i][^2..] == sotetVezerPoz || lepesek[i][^2..] == vilagosVezerPoz)) return true;
+                if (i % 2 == 0 && lepesek[i].Contains('V')) vilagosVezerPoz = lepesek[i][^2..];
+                else if (i % 2 == 1 && lepesek[i].Contains('V')) sotetVezerPoz = lepesek[i][^2..];
+            }
+
+            return false;
+        }
+        public bool VezerUtes => VezerLeutve();
+
+        public int Vezerlepesek => VezerLepesekSzama();
+
+
+        public int VezerLepesekSzama()
+        {
+            string vilagosVezerPoz = "d1";
+            string sotetVezerPoz = "d8";
+            int lepesekSzama = 0;
+            for (int i = 0; i < lepesek.Count(); i++)
+            {
+                if (i % 2 == 0 && lepesek[i].Contains('V'))
+                {
+                    if (vilagosVezerPoz[1] == lepesek[i][^1])
+                    {
+                        lepesekSzama += Math.Abs(dict[vilagosVezerPoz[0].ToString()] - dict[lepesek[i][^2].ToString()]);
+                    }
+
+
+                    vilagosVezerPoz = lepesek[i][^2..];
+                }
+                else if (i % 2 == 1 && lepesek[i].Contains('V'))
+                {
+                    sotetVezerPoz = lepesek[i][^2..];
+                }
+            }
+
+
+            return 10;
+        }
+
+
+
+
+
+    }
+}
---
+++ WpfSakk/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSakk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Jatszma> jatszmak;
+        public MainWindow()
+        {
+            InitializeComponent();
+            jatszmak = File.ReadAllLines("jatszmak.txt").Select(sor => new Jatszma(sor)).ToList();
+
+            MessageBox.Show($"A huszárok ennyi mezőt haladtak: {jatszmak.Sum(jatszma => jatszma.HuszarokLepesszama) * 4}");
+            MessageBox.Show($"A futók ennyiszer léptek: {jatszmak.Sum(jatszma => jatszma.TisztLepesszama('F'))}");
+            List<int> eredmenyek = new List<int>();
+            for (int i = 0; i < jatszmak.Count(); i++) if (jatszmak[i].VezerUtes) eredmenyek.Add(i + 1);
+            MessageBox.Show($"c: {string.Join(';', eredmenyek)}");
+            MessageBox.Show($"e: {jatszmak.Count(n => !n.FeherKiralyMozgas)}");
+            MessageBox.Show($"f: {jatszmak.Count(n=>n.BabukSzama>20)}");
+        }
+    }
+}
